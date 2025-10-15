3fa8ec815c5dbff7bf496b9211ab171d9fb400fb
Breno <breno.cpedroso@gmail.com>
2025-10-15 15:19:02 -0300

    lista 1 ex9 certo

--- lista_1_ex_9.c
+++ lista_1_ex_9.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 
 int main()
 {
     pid_t filhos[10];
+    srand(time(NULL));
 
     for (int i = 0; i < 10; i++)
     {
+        int numero_aleatorio = rand() % 10;
+        char buffer[12];
+        snprintf(buffer, sizeof(buffer), "%d\n", numero_aleatorio);
+        setenv("num", buffer, 1);
         filhos[i] = fork();
 
         if (filhos[i] == 0)
         {
-            printf("Mayoi! Quem quer dinheiro! 💵. Meu PID: %d\n", getpid());
+            char *mensagem = getenv("num");
+            printf("Sou o filho com PID: %d. Minha variável de ambiente tem o seguinte número: %s", getpid(), mensagem);
             exit(0);
         }
         else if (filhos[i] < 0)
@@ -23,7 +31,10 @@ int main()
         }
     }
 
-    sleep(1);
-    printf("Hello World!\n");
+    for (int i = 0; i < 10; i++)
+    {
+        wait(NULL);
+    }
+    
     return 0;
 }
